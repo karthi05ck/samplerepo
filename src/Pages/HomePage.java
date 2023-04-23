358b9a2ddf73922ba09a47c7f055f2c47bfed357
karthi05ck <karthickece05@gmail.com>
2023-04-23 18:42:02 +0530

    added,updated and deleted

--- src/Pages/HomePage.java
+++ src/Pages/HomePage.java
@@ -5,5 +5,6 @@ public class HomePage {
 	public static void main (String a[])
 	{
 		System.out.println("This is Home page");
+		String un = "abc";
 	}
 }
--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -5,5 +5,7 @@ public class LoginPage {
 	public static void main (String a[])
 	{
 		System.out.println("This is Login page");
+		
+		String pwd = "user";
 	}
 }
